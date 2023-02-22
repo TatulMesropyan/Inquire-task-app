e84ec0c43505e7b0b6b276c621cf554be44133de
t.mesropyan <Tatul.Mesropyan-extern@invia.de>
2023-02-23 00:01:06 +0400

    Init commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import SinglePost from "./components/SinglePost/SinglePost";
 
 function App() {
+    interface postTypes{
+        id:number,
+        title:string,
+        body: string,
+    }
+    const [posts,setPosts] = useState([])
+    useEffect(() => {
+    const getData = async () => {
+        const response = await axios.get('https://blog-api-t6u0.onrender.com/posts')
+        setPosts(response.data);
+    }
+        getData();
+    },[])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        {posts?.map(({title,body,id}) => (
+      <SinglePost
+          key={id}
+          title={title}
+          description={body}
+          postID={id}
+      />
+            ))}
     </div>
   );
 }
